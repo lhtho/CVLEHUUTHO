1f4f606cc6371f79c4a42382f3ad28f94deb5c9b
lhtho <thom5725024@gstudent.ctu.edu.vn>
2026-05-17 22:01:28 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+
+AOS.init({
+    duration:1200,
+    once:true
+});
+
+particlesJS("particles-js",{
+  particles:{
+    number:{value:70},
+    size:{value:3},
+    move:{speed:2},
+    line_linked:{enable:true},
+    color:{value:"#5eead4"}
+  }
+});
+
+new Typed('#typed',{
+    strings:[
+        'Senior Project Engineer',
+        'Construction Management Expert',
+        'AI Portfolio Developer'
+    ],
+    typeSpeed:50,
+    backSpeed:30,
+    loop:true
+});
